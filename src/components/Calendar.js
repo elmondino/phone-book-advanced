3c627d9cc51206a7d264ce6463c229f4b379b895
elmondino <elmonds@hotmail.co.uk>
2020-03-02 19:03:40 +0000

    initial commit

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,20 +1,43 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import './Calendar.css'
+import './Calendar.css';
+import { v1 as uuid } from 'uuid';
 
 class Calendar extends Component {
     state = {
         contacts: [],
         search: '',
-        sortType: 'asc'
+        sortType: 'asc',
+        contactName: '',
+        contactNumber: '',
+        contactAddress: ''
     }
     updateSearch = event => {
         this.setState({
             search: event.target.value
         })
     }
-    handleChange = (event) => {
-        this.setState({ sortType: event.target.value })
+    handleChange = (e) => {
+        this.setState({ sortType: e.target.value });
+    }
+    updateContact = (e) => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+    addContact = (e) => {
+        e.preventDefault();
+        this.setState({contacts: [...this.state.contacts, {name: this.state.contactName, address: this.state.contactAddress, id: uuid(), phone_number: this.state.contactNumber}]},
+         () => {
+            this.setState({
+                contactName: '',
+                contactAddress: '',
+                contactNumber: ''
+            })
+         });
+    }
+    removeContact = (id) => {
+        this.setState({
+            contacts: this.state.contacts.filter(contact => contact.id !== id)
+        })
     }
     componentDidMount() {
         axios.get('http://www.mocky.io/v2/581335f71000004204abaf83')
@@ -33,8 +56,8 @@ class Calendar extends Component {
             return contact.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
         });
         const sortedContacts = filteredContacts.sort((a, b) => {
-            const isReversed = (sortType === 'asc') ? 1 : -1;
-            return isReversed * a.name.localeCompare(b.name)
+            let isReversed = (sortType === 'asc') ? 1 : -1;
+            return isReversed * a.name.localeCompare(b.name);
         })
         const contactsList = sortedContacts.length ? (
             sortedContacts.map(contact => {
@@ -43,6 +66,7 @@ class Calendar extends Component {
                         <div>{contact.name}</div>
                         <div>{contact.phone_number}</div>
                         <div>{contact.address}</div>
+                        <div className="remove-contact" onClick={() => this.removeContact(contact.id)}>Remove Contact</div>
                         <hr/>
                     </div>
                 )
@@ -65,6 +89,14 @@ class Calendar extends Component {
                             <option value="desc">Descending</option>
                         </select>
                     </div> 
+                    <div className="add-contact">
+                        <form className="contact-form" onSubmit={this.addContact}>
+                            <input type="text" placeholder='name' name="contactName" value={this.state.contactName} onChange={this.updateContact} required />
+                            <input type="text" placeholder='phone number' name="contactNumber" value={this.state.contactNumber} onChange={this.updateContact} required />
+                            <input type="text" placeholder='address' name="contactAddress" value={this.state.contactAddress} onChange={this.updateContact} required />
+                            <input type="submit" value='Add Contact'/>
+                        </form>
+                    </div>
                 </div>
                 <div className="contact-list">
                     {contactsList}
